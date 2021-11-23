69990db371f422a19047acd0539d8b377053b5c3
Matthew King <matthewking@Matthews-MacBook-Pro.local>
2021-11-23 12:24:09 +0000

    Remove nested IF's and convert to guard clauses instead

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -23,64 +23,57 @@ class EmailController extends AbstractController
         $sendResult = false;
         $err = false;
 
-        if(isset($_POST['action']) && $_POST['action'] == 'send') {
-            if(!isset($_POST['email'])) {
-                $err = 'No email specified';
-            }
-            if(!isset($_POST['paw_address'])) {
-                $err = 'No PAW address specified';
-            }
-            if(!isset($_POST['paw_reward_key'])) {
-                $err = 'No reward key specified!';
-            }
-            if(!isset($_POST['terms'])) {
-                $err = 'Please check the vouch checkbox!';
-            }
-            $dbRewardKey = FALSE;
-            if(!$err) {
-                $dbRewardKey = $this->getDb()->db_reward_key($_POST['paw_reward_key']);
-                if(!$dbRewardKey) {
-                    $err = 'Reward Key Not Found';
-                } else if($dbRewardKey->disabled) {
-                    $err = 'Reward Key Suspended';
-                }
-            }
-
-            if(!$err) {
-                $emailInvited = $this->getDb()->db_email_invited($_POST['email']);
-                if($emailInvited) {
-                    $err = 'Email got invited already';
-                }
-
-            }
-
-            if(!$err) {
-                $lastInvite = $this->getDb()->db_last_email_invite($_POST['paw_reward_key']);
-                if ($lastInvite && (($lastInvite->time_invited + 60 * 10) > time())) {
-                    $err = 'Please wait 10 minutes between invites';
-                }
-            }
-
-            if(!$err) {
-                if(!Utils::validPawAddress($_POST['paw_address'])) {
-                    $err = 'Invalid PAW address entered';
-                }
-
-            }
-
-            if(!$err) {
-                $email = $_POST['email'];
-                $address = $_POST['paw_address'];
-                $pawRewardKey = $_POST['paw_reward_key'];
-                $pickupCode = Utils::randSha1(24);
-                $this->getDb()->db_insert_email_invite($email, $pawRewardKey, $pickupCode, $address);
-
-                $inviteeRewardKey = Utils::randSha1(24);
-                $this->getDb()->db_insert_reward_key($inviteeRewardKey, $dbRewardKey->id);
-
-                $sendResult = (new Email())->sendEmailInvite($_POST["email"], $inviteeRewardKey, $pickupCode);
-            }
+        if(!isset($_POST['action']) || $_POST['action'] != 'send') {
+            return $this->get($sendResult, $err);
         }
+
+        if (!isset($_POST['email'])) {
+            return $this->get($sendResult, 'No email specified');
+        }
+        if (!isset($_POST['paw_address'])) {
+            return $this->get($sendResult, 'No PAW address specified');
+        }
+        if (!isset($_POST['paw_reward_key'])) {
+            return $this->get($sendResult, 'No reward key specified!');
+        }
+        if (!isset($_POST['terms'])) {
+            return $this->get($sendResult, 'Please check the vouch checkbox!');
+        }
+        if ($err) {
+            return $this->get($sendResult, $err);
+        }
+
+        $dbRewardKey = $this->getDb()->db_reward_key($_POST['paw_reward_key']);
+        if (!$dbRewardKey) {
+            return $this->get($sendResult, 'Reward Key Not Found');
+        } else if($dbRewardKey->disabled) {
+            return $this->get($sendResult, 'Reward Key Suspended');
+        }
+
+        $emailInvited = $this->getDb()->db_email_invited($_POST['email']);
+        if ($emailInvited) {
+            return $this->get($sendResult, 'Email got invited already');
+        }
+
+        $lastInvite = $this->getDb()->db_last_email_invite($_POST['paw_reward_key']);
+        if ($lastInvite && (($lastInvite->time_invited + 60 * 10) > time())) {
+            return $this->get($sendResult, 'Please wait 10 minutes between invites');
+        }
+
+        if(!Utils::validPawAddress($_POST['paw_address'])) {
+            return $this->get($sendResult, 'Invalid PAW address entered');
+        }
+
+        $email = $_POST['email'];
+        $address = $_POST['paw_address'];
+        $pawRewardKey = $_POST['paw_reward_key'];
+        $pickupCode = Utils::randSha1(24);
+        $this->getDb()->db_insert_email_invite($email, $pawRewardKey, $pickupCode, $address);
+
+        $inviteeRewardKey = Utils::randSha1(24);
+        $this->getDb()->db_insert_reward_key($inviteeRewardKey, $dbRewardKey->id);
+
+        $sendResult = (new Email())->sendEmailInvite($_POST["email"], $inviteeRewardKey, $pickupCode);
         return $this->get($sendResult, $err);
     }
 }
